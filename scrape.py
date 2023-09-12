91bb35233b0705a1906e819f23e0db3cf063ac03
ABDOUL FARIDE BASSIROU ALZOUMA <abdoulfaridbassirou7898@gmail.com>
2023-09-12 20:09:19 +0300

    Hacker News Project 3

--- scrape.py
+++ scrape.py
@@ -7,6 +7,9 @@ soup = BeautifulSoup(res.text, 'html.parser')
 links = soup.select('.titleline > a')
 subtext = soup.select('.subtext')
 
+def sort_stories_by_votes(hnlist):
+	return sorted(hnlist,key= lambda k:k['votes'],reverse = True)
+
 def create_custom_hn(links,subtext):
 	hn = []
 	for idx,item in enumerate(links):
@@ -17,6 +20,6 @@ def create_custom_hn(links,subtext):
 		    points = int(vote[0].getText().replace(' points',''))
 		    if points > 99:
 		           hn.append({'title':title,'link':href,'votes':points})
-	return hn 
+	return sort_stories_by_votes(hn) 
 
 pprint.pprint(create_custom_hn(links,subtext))
