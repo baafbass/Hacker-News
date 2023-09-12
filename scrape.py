0cacc6fdd31405bb5115a611856c79efdf767c03
ABDOUL FARIDE BASSIROU ALZOUMA <abdoulfaridbassirou7898@gmail.com>
2023-09-12 18:05:40 +0300

    BeautifulSoup Selectors

--- scrape.py
+++ scrape.py
@@ -3,9 +3,5 @@ from bs4 import BeautifulSoup
 
 res = requests.get('https://news.ycombinator.com/news')
 soup = BeautifulSoup(res.text, 'html.parser')
-print(soup.find_all('div'))
-print(soup)
-print(soup.find('a')) # return the anchor it found
-print('')
-print(soup.body)
-print(soup.title)
+links = soup.select('.titleline')
+votes = soup.select('.score')
