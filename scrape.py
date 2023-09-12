b363670fadd1a45ebc7b65c33709409464275ecd
ABDOUL FARIDE BASSIROU ALZOUMA <abdoulfaridbassirou7898@gmail.com>
2023-09-12 19:44:29 +0300

    Hacker News Project 2

--- scrape.py
+++ scrape.py
@@ -1,22 +1,22 @@
 import requests
 from bs4 import BeautifulSoup
+import pprint
 
 res = requests.get('https://news.ycombinator.com/news')
 soup = BeautifulSoup(res.text, 'html.parser')
-links = soup.select('.titleline a')
-votes = soup.select('.score')
+links = soup.select('.titleline > a')
+subtext = soup.select('.subtext')
 
-
-def create_custom_hn(links,votes):
+def create_custom_hn(links,subtext):
 	hn = []
 	for idx,item in enumerate(links):
-		title = links[idx].getText()
-		href = links[idx].get('href',None)
-		points = int(votes[idx].getText().replace(' points',''))
-		print(points)
-		hn.append({'title':title,'link':href})
+		title = item.getText()
+		href = item.get('href',None)
+		vote = subtext[idx].select('.score')
+		if len(vote):
+		    points = int(vote[0].getText().replace(' points',''))
+		    if points > 99:
+		           hn.append({'title':title,'link':href,'votes':points})
 	return hn 
 
-#print(create_custom_hn(links,votes))
-
-create_custom_hn(links,votes)
+pprint.pprint(create_custom_hn(links,subtext))
